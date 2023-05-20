a22266ec3c495213bc638de75745f87a8888a20d
Paulo Henrique U. Leite <96631286+vondarten@users.noreply.github.com>
2023-05-20 18:40:31 -0300

    Add files via upload

---
+++ cadastro_loja/cadastro_loja.js
@@ -0,0 +1,29 @@
+function formatNumber(number) {
+    const cleanedNumber = number.replace(/\D/g, '');
+  
+    const isCpf = cleanedNumber.length === 11;
+  
+    const mask = isCpf ? '000.000.000-00' : '00.000.000/0000-00';
+  
+    let formattedNumber = '';
+    let index = 0;
+    for (let i = 0; i < mask.length; i++) {
+      if (mask[i] === '0') {
+        formattedNumber += cleanedNumber[index] || '';
+        index++;
+      } else {
+        formattedNumber += mask[i];
+      }
+    }
+  
+    return formattedNumber;
+  }
+
+function formatInputNumber() {
+  const numberInput = document.getElementById('login-cpf-cnpj');
+  const inputValue = numberInput.value;
+  
+  const formattedNumber = formatNumber(inputValue);
+  numberInput.value = formattedNumber;
+}
+
